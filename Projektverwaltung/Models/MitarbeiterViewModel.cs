ba53f32adbfe70de63a2e80d69648db27eed874b
iFL3x <fabrice-95@hotmail.com>
2018-03-07 16:24:42 +0100

    Comments

--- Projektverwaltung/Models/MitarbeiterViewModel.cs
+++ Projektverwaltung/Models/MitarbeiterViewModel.cs
@@ -1,4 +1,11 @@
-﻿
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MitarbeiterViewModel.cs" author="Fabrice Koenig">
+//   Copyright (c) Fabrice Koenig Ltd. All rights reserved.
+// </copyright>
+// <summary>
+//   ViewModel Class for the Employee Data. 
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 using Projektverwaltung.Database;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,21 +13,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Projektverwaltung.Models
 {
+    /// <summary>
+    /// The MitarbeiterViewModel class is used as a ViewModel for the EmployeeData inside the MitarbeiterController
+    /// </summary>
     public class MitarbeiterViewModel
     {
-        
+
+        [DisplayName("ID")]
         public int Id { get; set; }
-        
+
+        [DisplayName("Name")]
         public string Name { get; set; }
-        
+
+        [DisplayName("Vorname")]
         public string Vorname { get; set; }
         
+        [DisplayName("Abteilung")]
         public string Abteilung { get; set; }
 
         [DisplayName("Pensum")]
         [Range(1, 100)]
         public int Pensum { get; set; }
 
+        [DisplayName("Funktionen")]
         public IEnumerable<Funktion> Funktionen { get; set; }
     }
 }
